315bab838604172852362db29b5c7e5dc465866c
Madina <madinadzanaeva670@gmail.com>
2025-07-12 21:32:03 +0300

    Настроила среду React

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+// Здесь будет сборка компонентов
+
+import React, { Component } from "react";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Привет, мир!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
