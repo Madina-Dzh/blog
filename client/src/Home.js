cd314c81460e3f598fd9de8cd3bc103ab8e818cc
Madina <madinadzanaeva670@gmail.com>
2025-07-29 17:50:02 +0300

    Стилизация базового post

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import './Home.css';
+import Post from "./Post";
 
 class Home extends Component {
     render() {
@@ -27,51 +28,16 @@ class Home extends Component {
                         <div>Тег 5</div>
                         <div>Тег 6</div>
                     </div>
-                    <div>
-                        <div className="posts">
-                            <h3>Пост 1!</h3>
-                            <img src="https://imgholder.ru/323x300/9dbf16/xfff,atext=image.pagespeed.ic.h4y7-DvpPp.webp" alt="error" />
-                            <div>Место для тегов</div>
-                            <div>
-                                Место для текста поста
-                                Место для текста поста
-                                Место для текста поста
-                            </div>
-                            <div>
-                                <button>&#x2764;</button>
-                                <button>&#8629;</button>
-                            </div>
-                        </div>
-
-                        <div>
-                            <h3>Пост 2!</h3>
-                            <img src="https://imgholder.ru/323x300/9dbf16/xfff,atext=image.pagespeed.ic.h4y7-DvpPp.webp" alt="error" />
-                            <div>Место для тегов</div>
-                            <div>
-                                Место для текста поста
-                                Место для текста поста
-                                Место для текста поста
-                            </div>
-                            <div>
-                                <button>&#x2764;</button>
-                                <button>&#8629;</button>
-                            </div>
-                        </div>
-
-                        <div>
-                            <h3>Пост 3!</h3>
-                            <img src="https://imgholder.ru/323x300/9dbf16/xfff,atext=image.pagespeed.ic.h4y7-DvpPp.webp" alt="error" />
-                            <div>Место для тегов</div>
-                            <div>
-                                Место для текста поста
-                                Место для текста поста
-                                Место для текста поста
-                            </div>
-                            <div>
-                                <button>&#x2764;</button>
-                                <button>&#8629;</button>
-                            </div>
-                        </div>
+                    <div className="post-place">
+                        <Post />
+                        <Post />
+                        <Post />
+                        <Post />
+                        <Post />
+                        <Post />
+                        <Post />
+                        <Post />
+                        <Post />
                     </div>
                 </main>
 
